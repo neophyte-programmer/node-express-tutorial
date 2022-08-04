93236bd6629a6c86dd86cca650ca2db8c49370e5
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-04 15:50:41 +0000

    feat: :sparkles: globals
    learnt about global variables

---
+++ node-tutorial/01-globals.js
@@ -0,0 +1,17 @@
+// There is no window object in Node
+
+//  GLOBALS
+// They can be accessed anywhere in the application
+
+//  __dirname => gives path to current directory
+//  __filename => file name
+//  require => function to use modules
+//  module => gives info about current module/file
+//  process => gives info about environment where program is being executed
+
+// console.log(__dirname);
+
+setInterval(() => {
+    var number = 0
+    console.log(number)
+}, 1000)
