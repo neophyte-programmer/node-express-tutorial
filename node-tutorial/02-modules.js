587de053e783af7619a2c8da25944a340123a3cc
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-04 16:40:12 +0000

    feat: :sparkles: more on modules

--- node-tutorial/02-modules.js
+++ node-tutorial/02-modules.js
@@ -2,7 +2,10 @@
 // Modules are encapsulated code which share minimum stuff
 const names = require('./03-names');
 const greeting = require('./04-utils');
+const data = require('./05-alternative-syntax')
+require('./06-mindGrenade');
 
-console.log(names);
+// console.log(names);
+// console.log(data);
 
-greeting(names.john)
+// greeting(names.john)
---
+++ node-tutorial/05-alternative-syntax.js
@@ -0,0 +1,6 @@
+module.exports.items = ['item1', 'item2', 'item3']
+const person = {
+    name: "Nutifafa"
+}
+
+module.exports.singlePerson = person
